95dc29bb8eed7bc1a43272bab381cb77175dd9fe
Spanningtreegiant <greengiant12@hotmail.co.uk>
2025-09-28 13:30:39 +0100

    cleaning up

--- main.js
+++ main.js
@@ -1,19 +1,9 @@
-var bezos = document.getElementById('bezos');
-var bezos_counter = document.getElementById('bezos-counter');
-var bezosCounterStart = document.getElementById('bezos-counter-start');
+var arnault = document.getElementById('arnault');
+var arnault_counter = document.getElementById('arnault-counter');
+var arnaultCounterStart = document.getElementById('arnault-counter-start');
 
-var four_hundred = document.getElementById('four-hundred');
-var four_hundred_counter = document.getElementById('four-hundred-counter');
-var four_hundred_counter_start = document.getElementById('four-hundred-counter-start');
-
-var sixtyPercent = document.getElementById('sixty-percent');
-var sixtyPercentIndicator = document.getElementById('sixty-percent-indicator');
-var sixtyPercentScrollPercentage = 0.0;
-var babies = document.getElementById('babies-wrapper');
-var baby_counter = document.getElementById('baby-counter');
-
-var thousand = new Intl.NumberFormat('en-US')
-var money = new Intl.NumberFormat('en-US', {
+var thousand = new Intl.NumberFormat('fr-FR')
+var money = new Intl.NumberFormat('fr-FR', {
   style: 'currency',
   currency: 'EUR',
   minimumFractionDigits: 0,
@@ -25,63 +15,24 @@ window.addEventListener('scroll', function(){
   update_wealth_counter();
 });
 
-function generate_sixty_percent() {
-  for (var i = 0; i < 100; i++) {
-    var node = document.createElement("div");
-    node.classList = "people";
-    if (i === 0) {
-      node.classList += " first";
-    }
-    document.getElementById("sixty-percent").appendChild(node);
-  }
-}
-generate_sixty_percent();
-
-sixtyPercent.addEventListener('scroll', function(){
-  let newScroll = ((sixtyPercent.scrollTop / sixtyPercent.scrollHeight) * 60).toFixed(1);
-  if (sixtyPercentScrollPercentage !== newScroll) {
-    sixtyPercentScrollPercentage = newScroll;
-    sixtyPercentIndicator.innerHTML = newScroll + '%';
-  }
-})
-babies.addEventListener('scroll', function(){
-  let is_mobile = window.innerWidth <= 450;
-  let bg_size = (is_mobile) ? 68 : 160;
-  baby_counter.innerHTML = thousand.format(Math.floor(babies.scrollTop / bg_size * 5));
-})
-
 //Todo: stop executing once scrolled past
 function update_wealth_counter() {
-  if (bezos_viewable()) {
-    if (bezos_counter_viewable()) {
-      let wealth = (window.scrollX - bezos.offsetLeft + 175) * 500000;
-      bezos_counter.innerHTML = (wealth < 149000000000) ? money.format(wealth) : "$149,000,000,000.00";
+  if (arnault_viewable()) {
+    if (arnault_counter_viewable()) {
+      let wealth = (window.scrollX - arnault.offsetLeft + 175) * 500000;
+      arnault_counter.innerHTML = (wealth < 149000000000) ? money.format(wealth) : "149,000,000,000.00€";
     }
     else {
-      bezos_counter.innerHTML = '';
+      arnault_counter.innerHTML = '';
     }
   }
-  else if (four_hundred_viewable()) {
-    if (four_hundred_counter_viewable()) {
-      let wealth = (window.scrollX - four_hundred.offsetLeft + 175) * 500000;
-      four_hundred_counter.innerHTML = (wealth < 2960000000000) ? money.format(wealth) : "$2,960,000,000,000.00";
-    }
-    else {
-      four_hundred_counter.innerHTML = '';
-    }
+  function arnault_viewable() {
+    return window.scrollX < arnault.offsetLeft + arnault.offsetWidth + 100;
   }
-  function bezos_viewable() {
-    return window.scrollX < bezos.offsetLeft + bezos.offsetWidth + 100;
+  function arnault_counter_viewable() {
+    return arnaultCounterStart.offsetLeft - window.scrollX < (window.innerWidth);
   }
-  function bezos_counter_viewable() {
-    return bezosCounterStart.offsetLeft - window.scrollX < (window.innerWidth);
-  }
-  function four_hundred_viewable() {
-    return window.scrollX < four_hundred.offsetLeft + four_hundred.offsetWidth + 100;
-  }
-  function four_hundred_counter_viewable() {
-    return four_hundred_counter_start.offsetLeft - window.scrollX < (window.innerWidth);
-  }
+
 }
 function toggleZoom() {
   document.getElementById('line-chart').classList.toggle('zoom');
